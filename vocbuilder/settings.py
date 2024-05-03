b67fccd80cd86d23d6ea36f41c4535c349e678a1
danil <vinniopo@mail.ru>
2024-05-03 19:18:45 +0700

    database, requirements, gitignore

--- vocbuilder/settings.py
+++ vocbuilder/settings.py
@@ -74,9 +74,11 @@ WSGI_APPLICATION = 'vocbuilder.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "read_default_file": "vocbuilder/my.cnf",
+        },
     }
 }
 
