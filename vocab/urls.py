2541dad65987d4701bc015ed886b5024d7f3fda1
danil <vinniopo@mail.ru>
2024-05-05 11:19:39 +0700

    added templates

--- vocab/urls.py
+++ vocab/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-# urlpatterns = [
-#     path("", views.index, name="index"),
-# ]
+app_name = "vocab"
+urlpatterns = [
+    path("", views.index, name="index"),
+]
--- vocbuilder/settings.py
+++ vocbuilder/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'vocab',
+    # 'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# INTERNAL_IPS = [
+#     "127.0.0.1",
+# ]
