916e7faf451aa258fcdd1051384b3a5fa0b211d6
danil <vinniopo@mail.ru>
2024-05-06 11:55:48 +0700

    vocab models and migrations

--- vocab/models.py
+++ vocab/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+
+class Authors(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+
+
+class Books(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False)
+    authors = models.ForeignKey(Authors, on_delete=models.CASCADE)
+
+
+class Dictionary(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+    language_from = models.CharField(max_length=2, null=False, blank=False)
+    language_to = models.CharField(max_length=2, null=False, blank=False)
+
+
+class Word(models.Model):
+    stem = models.CharField(max_length=200, null=False, blank=False)
+
+
+class Usage(models.Model):
+    word = models.ForeignKey(Word, on_delete=models.CASCADE)
+    context_word = models.CharField(max_length=200, null=False, blank=False)
+    usage = models.TextField(blank=False, null=False)
+
+
+class DictRecord(models.Model):
+    dictionary = models.ForeignKey(Dictionary, on_delete=models.CASCADE)
+    pronunciation = models.CharField(max_length=200, null=True)
+    did_you_know_header = models.CharField(max_length=100, null=True)
+    did_you_know = models.TextField(null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Definition(models.Model):
+    dict_record = models.ForeignKey(DictRecord, on_delete=models.CASCADE)
+    part_of_speech = models.CharField(max_length=100)
+
+
+class SenseSequence(models.Model):
+    definition = models.ForeignKey(Definition, on_delete=models.CASCADE)
+    role = models.CharField(max_length=200, null=True)
+
+
+class Sense(models.Model):
+    sense_sequence = models.ForeignKey(SenseSequence, on_delete=models.CASCADE)
+    definition = models.TextField(null=False, blank=False)
+    example = models.CharField(max_length=200, null=True)
+    labels = models.CharField(max_length=200, null=True)
+    usage_notes = models.CharField(max_length=200, null=True)
+
+
+class Etymology(models.Model):
+    dict_record = models.ForeignKey(DictRecord, on_delete=models.CASCADE)
+    part_of_speech = models.CharField(max_length=100)
+    etymology = models.TextField(null=False, blank=False)
+
+
+class ExampleUsage(models.Model):
+    dict_record = models.ForeignKey(DictRecord, on_delete=models.CASCADE)
+    text = models.TextField(null=False, blank=False)
+
+
+class Derived(models.Model):
+    dict_record = models.ForeignKey(DictRecord, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200, null=False, blank=False)
+    pronunciation = models.CharField(max_length=200, null=True)
+    part_of_speech = models.CharField(max_length=100, null=True)
