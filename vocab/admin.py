8de95d0dbdf7effbf1343be4ae610153c27f6722
danil <vinniopo@mail.ru>
2024-05-06 18:20:14 +0700

    added dictionary to admin

--- vocab/admin.py
+++ vocab/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import (
+    Authors, Book, Definition, Dictionary, DictRecord, Derived, Etymology,
+    ExampleUsage, Inflection, Sense, SenseSequence, Usage, Word
+)
 
-# Register your models here.
+
+@admin.register(Dictionary)
+class DictionaryAdmin(admin.ModelAdmin):
+    pass
