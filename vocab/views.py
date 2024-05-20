cd846dcdedadede056aeba7e89985ca581ee5905
danil <vinniopo@mail.ru>
2024-05-20 21:33:48 +0700

    added logging and fail check to word_definition view

--- vocab/views.py
+++ vocab/views.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 import traceback
 import uuid
@@ -7,6 +8,9 @@ from django.conf import settings
 from django.shortcuts import redirect, render
 from .utils import (is_sqlite, read_db, commit_to_db, create_vocab_objects,
                     get_definitions, get_ranking)
+
+logger = logging.getLogger('IMAGE_SCRAPER')
+logging.basicConfig(encoding='utf-8', level=logging.INFO)
 
 
 def index(request: HttpRequest):
@@ -36,7 +40,7 @@ def index(request: HttpRequest):
 
 
 def word_definition(request: HttpRequest):
-    print(request.POST)
+    logger.info(f'requested word definition with POST data {request.POST}')
     word = request.POST['word']
     word_id = request.POST['word_id']
     db_name = request.POST['db_name']
@@ -49,18 +53,23 @@ def word_definition(request: HttpRequest):
         'word_id': word_id,
         'failed': True
     }
+    logger.info(f'db path is {file_path}')
     records = read_db(file_path, word=word)
+    logger.info(f'total records in db is {len(records)}')
     for record in records:
+        logger.info('processing record')
         word, title, authors, word_ctx, usage, id_ = record
         try:
             dict_record, _ = commit_to_db(word, title, authors, word_ctx, usage)
             if dict_record.error_fetching:
+                logger.error(f'Failed fetching the record')
                 return JsonResponse(error_message)
         except:
-            print(traceback.format_exc())
+            logger.error(traceback.format_exc())
             return JsonResponse(error_message)
     scraped = create_vocab_objects(word, dict_record)
     if not scraped:
+        logger.error('Error in commiting the definition to db')
         return JsonResponse(error_message)
     word_data = get_definitions(records)['words'][word]
     data = {
@@ -73,4 +82,7 @@ def word_definition(request: HttpRequest):
         'etymologies': word_data['etymologies'],
         'examples': word_data['examples']
     }
+    if not data['definitions']:
+        data['failed'] = True
+    logger.info(f'returning JSON response with data: {data}')
     return JsonResponse(data)
