af1b7216a58562a5786a5e0e9375894c509b6a6c
danil <vinniopo@mail.ru>
2024-05-23 00:50:14 +0700

    added delete button

--- vocab/static/vocab/js/components/card.js
+++ vocab/static/vocab/js/components/card.js
@@ -104,8 +104,17 @@ export const Card = React.memo(function ({rec_name, to_show, d}) {
                 >
             <span id={`anchor_${rec_idx}`} className="anchor"></span>
             <div key={1} className="word_card shadow col-md-12 col-lg-6 border rounded-2">
-                <h3 className = "text-center" key="name">{rec_name}</h3>
-                <em><p className = "text-center" key="pronunciation">{cardData['pron']}</p></em>
+                <div className="d-flex justify-content-center">
+                    <div className="d-flex flex-column ms-auto">
+                        <h3 className = "text-center" key="name">{rec_name}</h3>
+                        <em><p className = "text-center" key="pronunciation">{cardData['pron']}</p></em>
+                    </div>
+                    <button className="btn btn-outline-danger border-0  ms-auto align-self-start" type="button">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#dc3545" className="bi bi-trash3 overflow-visible" viewBox="0 0 16 16">
+                            <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+                        </svg>
+                    </button>
+                </div>
                 <ul key="usages">
                     { usages }
                 </ul>
@@ -127,7 +136,7 @@ export const Card = React.memo(function ({rec_name, to_show, d}) {
                     <div key={2} className="container d-flex justify-content-center">
                         <div className="container d-flex justify-content-center">
                         <div key={1}>
-                            <button onClick = {etiCollapse} className="btn btn-primary word_card_button" type="button" data-target={`#eti_${rec_idx}`} disabled={!eties.length} >
+                            <button onClick={etiCollapse} className="btn btn-primary word_card_button" type="button" data-target={`#eti_${rec_idx}`} disabled={!eties.length} >
                                 Etymology
                             </button>
                         </div>
