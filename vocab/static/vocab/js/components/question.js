dd83f8c0ca06d7cd35b9a6fd987597c6711dbc14
danil <vinniopo@mail.ru>
2024-05-22 09:07:39 +0700

    quiz

--- vocab/static/vocab/js/components/question.js
+++ vocab/static/vocab/js/components/question.js
@@ -1,38 +1,60 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import _ from "lodash";
 import { composeDefinitions } from './utils';
 
 
+
 export const Question = function({word}) {
-    console.log(word)
     const [inputValue, setInputValue] = React.useState('')
     const [success, setSuccess] = React.useState(false)
-
-    let use_record = _.sample(db_data.words[word].usages)
-    let context = use_record.context;
-    let usage = use_record.usage;
+    const [show, setShow] = React.useState(false)
+    const [firstLetter, setFirstLetter] = React.useState('')
+    const use_record = React.useRef(_.sample(db_data.words[word].usages))
+    
+    let context = use_record.current.context;
+    let usage = use_record.current.usage;
     let index = usage.indexOf(context);
     let usage_begin = usage.substring(0, index);
     let ctx = usage.substring(index, index + context.length);
     let usage_end = usage.substring(index + context.length)
     let definitions = composeDefinitions(db_data.words[word].definitions)
 
+    React.useEffect(
+        () => {
+            const popover = new bootstrap.Popover(document.querySelector(`#question_${db_data.words[word]['word_id']} [data-bs-toggle="popover"]`),
+                                        {html: true,
+                                         content: () => document.querySelector(`#qd_${db_data.words[word]['word_id']}`).outerHTML,
+                                         customClass: 'bg-secondary-subtle shadow'})
+            return () => popover.hide()
+        }
+    )
+    
 
-    return (
+    return (<>
             <div id={`question_${db_data.words[word]['word_id']}`}  key={db_data.words[word]['word_id']} className="card ms-2 shadow col-4">
                 <div className="card-body  d-flex flex-column">
                     <h5 className="card-title"></h5>
+                    <button onClick={() => setShow(true)} type="button" className="btn btn-link ms-auto" data-bs-container="body">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
+                            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
+                        </svg>
+                    </button>
                     <p className="card-text">
                         {usage_begin}
                         <strong>
                             {success
                             ? 
                             <span className='bg-success-subtle rounded'>{ctx}</span>
+                                :
+                                show
+                                ?
+                                <span className='bg-warning-subtle rounded'>{ctx}</span>
                             :
                             <input 
                                 onInput={(e) => {
                                     let value = e.target.value
-                                    console.log(value, ctx)
                                     if (value.length > ctx.length){
                                         setInputValue(value.slice(0, ctx.length))
                                     }
@@ -41,30 +63,44 @@ export const Question = function({word}) {
                                             setInputValue(value)
                                             setSuccess(true)
                                         }}
+                                    else{
+                                        setInputValue(value)
+                                        }
                                     }
                                 }
+                                onFocus={(e) => {
+                                    if (firstLetter && e.target.value.length == 0) {setInputValue(firstLetter)}
+                                }}
                                 className={`text-center bg-info-subtle border-0 rounded`}
+                                size={ctx.length}
                                 id={'quiz_' + db_data.words[word]['word_id']}
-                                placeholder={'*'.repeat(ctx.length)}
-                                style={{width: `${ctx.length * 12}px`}}>
+                                value={inputValue}
+                                placeholder={firstLetter + '*'.repeat(ctx.length)}
+                                style={{width: `${ctx.length * 12}px`}}
+                                >
                             </input>
                             }
                         </strong>
                         {usage_end}
                     </p>
-                    <p className="blockquote-footer">{use_record.book.title}</p>
+                    <p className="blockquote-footer">{use_record.current.book.title}</p>
                     <div className="mt-auto d-flex">
                         <button type="button" className="btn btn-link" onClick={() => {
-                            const input_el = document.querySelector(`#quiz_${db_data.words[word]['word_id']}`);
-                            const first_letter = ctx.slice(0, 1)
-                            input_el.dataset.firstLetter = first_letter
-                            input_el.value = first_letter
+                            setFirstLetter(ctx.slice(0, 1))
+                            setInputValue('')
                         }}>First letter?</button>
-                        <button type="button" className="btn btn-link ms-auto" data-bs-container="body">
+                        <button type="button" className="btn btn-link ms-auto" data-bs-container="body"
+                        data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content="lol">
                              Show definition
                         </button>
                     </div>
                 </div>
             </div>
+            {
+            ReactDOM.createPortal(<div id={`qd_${db_data.words[word]['word_id']}`}>
+                {definitions.length ? definitions : 'No definition yet'}
+                </div>, document.querySelector('#hidden'))
+            }
+        </>
     )
 }
--- vocab/static/vocab/js/components/quiz.js
+++ vocab/static/vocab/js/components/quiz.js
@@ -5,32 +5,47 @@ import _ from "lodash";
 const quiz_link = document.querySelector('#quiz_link') 
 
 export const Quiz = function() {
+    console.log('rendering quiz')
+    const [words, setWords] = React.useState(_.sampleSize(Object.keys(db_data.words), 3))
+    const myModal = React.useRef(0);
+
 
     React.useEffect(() => {
-        const myModal = new bootstrap.Modal(document.querySelector('#quiz_modal'))
-        const show_modal = () => myModal.show() 
+        myModal.current = new bootstrap.Modal(document.querySelector('#quiz_modal'))
+        const show_modal = (e) => {
+            e.preventDefault();    
+            myModal && myModal.current.show()
+        }
         quiz_link.addEventListener('click', show_modal)
         return () => quiz_link.removeEventListener('click', show_modal)
-    })
+    }, [])
+
+    function closeModal () {myModal && myModal.current.hide()}
 
     return (
-        <div class="modal" id="quiz_modal" tabindex="-1">
-            <div class="modal-dialog modal-xl" >
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title text-center w-100">Quiz</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <div className="modal" id="quiz_modal" tabindex="-1">
+            <div className="modal-dialog modal-xl" >
+                <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title text-center w-100">Quiz</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
+                     onClick={closeModal}></button>
                 </div>
-                <div class="modal-body d-flex justify-content-center p-4">
-                    {_.sampleSize(Object.keys(db_data.words), 3).map((word, idx) => {
-                        return <Question key={idx} word={word} />
+                <div className="modal-body d-flex justify-content-center p-4">
+                    {words.map((word, idx) => {
+                        return <Question key={db_data.words[word]['word_id']} word={word} />
                             }
                         )
                     }
                 </div>
-                <div class="modal-footer">
+                <div className="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Submit</button>
+                    <button id="refresh_quiz" type="button" class="btn btn-success" onClick={() => setWords(_.sampleSize(Object.keys(db_data.words), 3))}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/>
+                        <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"/>
+                        </svg>
+                    </button>
                 </div>
                 </div>
             </div>
